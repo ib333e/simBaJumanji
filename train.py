b8fe6be366672f64301878f598e4bf8fb1909a1f
ib333e <aa6162@princeton.edu>
2025-04-20 20:52:48 -0400

    added timers

--- train.py
+++ train.py
@@ -5,6 +5,7 @@ from jumanji.training import utils
 from jumanji.training.setup_train import (
     setup_env, setup_agent, setup_evaluators, setup_training_state
 )
+from jumanji.training.timer import Timer
 from jumanji.training.types import TrainingState
 from typing import Dict, Tuple
 
@@ -47,8 +48,15 @@ cfg = OmegaConf.create({
 key, init_key = jax.random.split(jax.random.PRNGKey(cfg.seed))
 env = setup_env(cfg=cfg)
 agent = setup_agent(cfg=cfg, env=env)
-stochastic_eval, greedy_eval = setup_evaluators(cfg=cfg, agent=agent)
+stochastic_eval, _ = setup_evaluators(cfg=cfg, agent=agent)
 training_state = setup_training_state(env=env, agent=agent, key=init_key)
+num_steps_per_epoch = (
+    cfg.env.training.n_steps
+    * cfg.env.training.total_batch_size
+    * cfg.env.training.num_learner_steps_per_epoch
+)
+eval_timer = Timer(out_var_name="metrics")
+train_timer = Timer(out_var_name="metrics", num_steps_per_timing=num_steps_per_epoch)
 
 def epoch_function(training_state: TrainingState) -> Tuple[TrainingState, Dict]:
     training_state, metrics = jax.lax.scan(
@@ -63,22 +71,18 @@ epoch_fn = jax.pmap(epoch_function, axis_name="devices")
 
 for epoch in range(cfg.env.training.num_epochs):
     # Eval
-    key, stochastic_key, greedy_key = jax.random.split(key, 3)
+    key, eval_key = jax.random.split(key)
     
-    stoch_eval = stochastic_eval.run_evaluation(training_state.params_state, stochastic_key)
-    jax.block_until_ready(stoch_eval)
-    stoch_metrics = utils.first_from_device(stoch_eval)
-    
-    greed_eval = greedy_eval.run_evaluation(training_state.params_state, greedy_key)
-    jax.block_until_ready(greed_eval)
-    greed_metrics = utils.first_from_device(greed_eval)
-    print("Epoch", epoch, "Stoch Eval:", stoch_metrics, "Greedy Eval:", greed_metrics)
+    with eval_timer:
+        eval = stochastic_eval.run_evaluation(training_state.params_state, eval_key)
+        jax.block_until_ready(eval)
+        metrics = utils.first_from_device(eval)
+    print("Epoch", epoch, "Evaluation:", metrics)
     # probably dump to json here
 
     # Train
-    state, train_metrics = epoch_fn(training_state)
-    jax.block_until_ready((state, train_metrics))
-    train_metrics = utils.first_from_device(train_metrics)
-    print("Epoch", epoch, "Train:", train_metrics)
-
-
+    with train_timer:
+        state, train_metrics = epoch_fn(training_state)
+        jax.block_until_ready((state, train_metrics))
+        metrics = utils.first_from_device(train_metrics)
+    print("Epoch", epoch, "Train:", metrics)
